5eb73dc6e6678b465026a9a225bf5951511ea7f8
Junior <ag.junior91@gmail.com>
2024-06-28 17:59:54 -0300

    subindo projeto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>Olá Professora Andressa!</h1>
+     <h2>Entrega 1 concluída, nos vemos na próxima :)</h2>
+  </div>
+
+);
